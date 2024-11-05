2ae41022a39ae439b4ec014a46dc93f8b0070633
jathurshandev <jathurshandev@gmail.com>
2024-11-05 11:33:45 +0100

    feat routes url

---
+++ front/src/Routes/Routes.ts
@@ -0,0 +1,22 @@
+// Backend url //
+export const API_URL = "http://localhost:3000";
+
+// Auth routes //
+export const AUTH_REGISTER = API_URL + "/auth/register"
+export const AUTH_LOGIN = API_URL + "/auth/login";
+export const AUTH_LOGOUT = API_URL + "/auth/logout";
+export const AUTH_ME = API_URL + "/auth/me";
+
+// Chat/Messages routes //
+export const CHAT_SEND = (messageId: string) => API_URL + `/chat/${messageId}/send`;
+export const CHAT_DELETE = (messageId: string) => API_URL + `/chat/${messageId}`;
+export const MESSAGES_GET = (userId: string) => API_URL + `/messages/${userId}`;
+
+// Social routes //
+export const SOCIAL_SEND_FRIEND_REQUEST = (requestId: string) => API_URL + `/social/friend-request/${requestId}`;
+export const SOCIAL_ACCEPT_FRIEND_REQUEST = (requestId: string) => API_URL + `/social/friend-request/${requestId}/accept`;
+export const SOCIAL_GET_FRIEND_REQUESTS = API_URL + "/social/friend-request";
+export const SOCIAL_GET_FRIENDS = API_URL + "/social/friends";
+
+// Notification routes
+export const NOTIFICATIONS = API_URL + "/notifications";
