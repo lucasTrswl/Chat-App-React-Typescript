869acd1590df9b69111d3c404f1876d5ba44239a
B.Florian <floflobarroso@gmail.com>
2024-11-06 09:48:03 +0100

    add redirection to friendsRequest

--- front/src/components/ConversationList.tsx
+++ front/src/components/ConversationList.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { AuthService } from "../Services/AuthService"; // Adjust path as necessary
+import { useNavigate } from "react-router-dom";
+import friendsRequest from '../img/friendsRequest.png';
+import imgValidate from "../img/valide.png";
 
 type Conversation = {
   id: number;
@@ -13,27 +17,53 @@ const conversations: Conversation[] = [
   { id: 3, name: "Marie",  time: "12:30 PM" },
 ];
 
+
 export default function ConversationsList() {
+
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    const success = await AuthService.Logout();
+
+    if(success) {
+      console.log("Déconnexion réussie");
+      navigate("/login")
+    } else {
+      console.log("Erreur")
+    }
+  };
+
   return (
-    <div className="w-80 h-screen bg-gray-100 border-r">
-      <h2 className="p-4 text-2xl font-semibold text-gray-700 border-b">Conversations</h2>
-      <div className="overflow-y-auto">
-        {conversations.map((conversation) => (
-          <div
-            key={conversation.id}
-            className="flex items-center justify-between p-4 hover:bg-gray-200 cursor-pointer transition-colors duration-200"
-          >
-            <div>
-              <h3 className="text-lg font-medium text-gray-800">{conversation.name}</h3>
-            </div>
-            <div className="text-right">
-              <p className="text-xs text-gray-400">Last message: {conversation.time}</p>
-              
-              
-            </div>
+      <div className="flex flex-col w-80 h-screen bg-gray-100 border-r">
+          <div className="flex flex-row justify-between w-full">
+              <h2 className="p-4 text-2xl font-semibold text-gray-700 border-b">Conversations</h2>
+              <button type="button"
+                      className="p-4 hover:scale-110 transition-transform pl-5"
+                      onClick={() => navigate("/friendsRequest")}>
+                  <img
+                      alt="accept"
+                      src={friendsRequest}
+                      className="w-9 h-9 mx-auto"
+                  />
+              </button>
           </div>
-        ))}
+          <div className="overflow-y-auto flex-grow">
+              {conversations.map((conversation) => (
+                  <div
+                      key={conversation.id}
+                      className="flex items-center justify-between p-4 hover:bg-gray-200 cursor-pointer transition-colors duration-200"
+                  >
+                      <div>
+                          <h3 className="text-lg font-medium text-gray-800">{conversation.name}</h3>
+                      </div>
+                      <div className="text-right">
+                          <p className="text-xs text-gray-400">Last message: {conversation.time}</p>
+                      </div>
+                  </div>
+              ))}
+          </div>
+          <button type="button" onClick={handleLogout} className="p-4 w-full  bg-red-500 cursor-pointer hover:text-white
+      ">Se déconnecter
+          </button>
       </div>
-    </div>
   );
 }
